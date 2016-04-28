cae6aac7c0433b062b2f0b1c24199bf3262bf6be
Dasia <dasiawilliams2016@gmail.com>
2016-04-28 16:34:02 +0000

    class thing

--- index.js
+++ index.js
@@ -1,57 +1,69 @@
 // noprotect
 alert('think of a number between 1 and 63');
 
-  var min = 0;
+var min = 0;
 
-  var max = 64;
+var max = 64;
 
-  var guess = (min+max)/2;
+var guess = (min + max) / 2;
 
-  var guesses = 6;
+var guesses = 6;
 
-var comguess = prompt ('Min is ' +min+ 'Max is ' +max+ 'i guessed '+guess+' is it too High(H) too low (L) correct (C)');
-  comguess=comguess.toUpperCase(); 
+var comguess = prompt('max is ' + max + ' min is ' + min + ' i guessed ' + guess + ' is it too High(H) too low (L) correct (C)');
 
-while (guesses!==0){
-  
-   if (comguess === 'L'){ 
- 
-      min = guess;
-    var guess = (guess+max)/2; 
-    //var guess = max;
-    
-    var comguess = prompt ( 'Min is ' +min+ 'Max is ' +max+ '. I guessed '+ guess+' is it too High(H) too low (L) correct (C)');
-  comguess=comguess.toUpperCase(); 
-  
-  }
-  else if (comguess === 'H'){
-         max = guess;
-      var  guess = (min+guess)/2;
-   //var guess = min;
-     
-      var comguess = prompt ('Min is ' +min+ 'Max is ' +max+ 'i guessed '+ guess+' is it too High(H) too low (L) correct (C)');
-  comguess=comguess.toUpperCase(); 
-  }
-  else if (comguess === ''){
-  
-  var comguess = prompt ("could you please enter H C or L");
-   }
 
-  else if (comguess === 'C'){
-  
-   alert("i've won!");
-   break;
-   }
-  
-  else   {
-    alert("you've cheated!");
-  break;
-    
- }
+console.log(comguess);
+comguess = comguess.toUpperCase();
 
-//to stop guessing 
-  guesses--;
-  //no crazy amount of guesses
+while (guesses !== 0) {
+
+    if (comguess === 'L') {
+
+        min = guess;
+        guess = (guess + max) / 2;
+        //var guess = max;
+
+        comguess = prompt('max is ' + max + ' min is ' + min + ' I guessed ' + guess + ' is it too High(H) too low (L) correct (C)');
+        comguess = comguess.toUpperCase();
+
+        if (max - guess == 1) {
+            alert("you've cheated! low ");
+            break;
+        }
+
+    }
+    else if (comguess === 'H') {
+        max = guess;
+        guess = (min + guess) / 2;
+        //var guess = min;
+
+        comguess = prompt('max is ' + max + ' min is ' + min + ' i guessed ' + guess + ' is it too High(H) too low (L) correct (C)');
+        comguess = comguess.toUpperCase();
+
+        if (guess - min == 1) {
+            alert("you've cheated! high");
+            break;
+
+        }
+    }
+
+
+    else if (comguess === 'C') {
+
+        alert("i've won!");
+        break;
+    }
+
+    else {
+        comguess = prompt("could you please enter H C or L");
+        comguess = comguess.toUpperCase();
+        console.log(comguess);
+
+    }
+
+    //to stop guessing 
+    guesses--;
+    //no crazy amount of guesses
 
 
 
@@ -64,4 +76,3 @@ while (guesses!==0){
 
 
 }
-  
